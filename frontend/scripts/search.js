e87cd6077b20ef3c4e1a8a73c489fbd22def7384
ucyo <cayoglu@me.com>
2020-12-24 13:23:18 +0100

    Abstract to function and apply to other values

--- frontend/scripts/search.js
+++ frontend/scripts/search.js
@@ -1,16 +1,25 @@
+
+
+function outputFuse(fuse, iid, oid, arr) {
+    document.getElementById(iid).addEventListener("input", function(e) {
+        let result = fuse.search(this.value);
+        document.getElementById(oid).innerHTML = "";
+        result.forEach(function(item) {
+            var namelist = "<li>" + arr[item.refIndex] + "</li>";
+            document.getElementById(oid).innerHTML += namelist;
+        });
+    });
+}
+
 import Fuse from 'https://cdn.jsdelivr.net/npm/fuse.js@6.4.3/dist/fuse.esm.js'
 const options = {
     includeScore: true,
     minMatchCharLength: 2,
     threshold: .4
 };
-const fuse = new Fuse(countries, options);
 
-document.getElementById("search").addEventListener("input", function(e) {
-    let result = fuse.search(this.value);
-    document.getElementById("searchresults").innerHTML = "";
-    result.forEach(function(item) {
-        var namelist = "<li>" + countries[item.refIndex] + "</li>";
-        document.getElementById("searchresults").innerHTML += namelist;
-    });
-});
+const fuse_countries = new Fuse(countries, options);
+outputFuse(fuse_countries, "search", "searchresults", countries)
+
+const fuse_regions = new Fuse(regions, options);
+outputFuse(fuse_regions, "regions", "detailedsearchresults", regions)

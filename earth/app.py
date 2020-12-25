48ed284489caff579212975949bc6f56862a1a5d
ucyo <cayoglu@me.com>
2020-12-25 18:35:19 +0100

    Add some styling to the webpages

--- earth/app.py
+++ earth/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask.json import jsonify
 from glob import glob
 from .validate_yaml import get_yaml
@@ -14,20 +14,36 @@ for x in yaml_files:
     entry["id"] = x
     db.append(entry)
 
+
+def _uniques(key):
+    result = set()
+    for x in db:
+        result = set(list(result) + x[key])
+    return result
+
+
 @app.route('/')
 def hello_world():
-    return jsonify(db)
+    return render_template("index.html", db=db)
 
 
 @app.route('/search')
 def search():
-    return jsonify(request.args)
+    req = request.args
+    return render_template("search.html")
 
 
 @app.route('/detailedsearch')
-def detailedsearch():
-    return jsonify(request.args)
+def detailedsearch(rights=None):
+    rights = _uniques('rights')
+    return render_template("detailedsearch.html", rights=rights)
 
+
+@app.route('/dataset/<dataset>')
+def dataset(dataset):
+    path = os.path.join(catalog_location, dataset+'.yml')
+    meta = get_yaml(path)
+    return jsonify(meta)
 
 @app.errorhandler(404)
 def page_not_found(error):
